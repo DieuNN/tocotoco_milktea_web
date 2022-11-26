ffe0179192a28a231880dc28849fc53e816a8342
dieu <nongngocdieu20122002@gmail.com>
2022-11-26 17:46:55 +0700

    Fix small bugs

--- src/config/posgre.ts
+++ src/config/posgre.ts
@@ -8,17 +8,17 @@ dotenv.config({
 
 
 export const PostgreSQLConfig: ClientConfig = {
-    host: process.env.HOST,
-    port: Number(process.env.PORT),
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    ssl: {
-        rejectUnauthorized: true,
-        cert: fs.readFileSync('./cert.pem').toString(),
-        key: fs.readFileSync('./key.pem').toString()
-    },
+    // host: process.env.HOST,
+    // port: Number(process.env.PORT),
+    // database: process.env.DATABASE,
+    // user: process.env.USER,
+    // password: process.env.PASSWORD,
+    // ssl: {
+    //     rejectUnauthorized: true,
+    //     cert: fs.readFileSync('./cert.pem').toString(),
+    //     key: fs.readFileSync('./key.pem').toString()
+    // },
         // query_timeout: 60000,
         // connectionTimeoutMillis: 60000,
-    // connectionString: "postgres://dieu:6j52V96LmusXlpiXZTzVKQtR1QoXDb2M@dpg-cdbb2nqrrk09hiqcif50-a.singapore-postgres.render.com/tocotea?ssl=true",
+    connectionString: "postgres://dieu:6j52V96LmusXlpiXZTzVKQtR1QoXDb2M@dpg-cdbb2nqrrk09hiqcif50-a.singapore-postgres.render.com/tocotea?ssl=true",
 }
